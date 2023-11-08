91643f9d7fffca9a0b9b0cdcfd86ebf8530d4175
Sebastian Gomez Quintero <sebastiangomezq10@gmail.com>
2023-11-08 00:36:20 -0500

    Se cabia informacion del footer para visuaizacion

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../Assets/Logo.svg";
+import Logo from "../Assets/variacion1-positivo@2x.png";
 import { BsTwitter } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
@@ -30,9 +30,9 @@ const Footer = () => {
         </div>
         <div className="footer-section-columns">
           <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <span>hello@nudolab.com</span>
+          <span>press@nudolab.com</span>
+          <span>contact@nudolab.com</span>
         </div>
         <div className="footer-section-columns">
           <span>Terms & Conditions</span>
